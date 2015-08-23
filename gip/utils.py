454106747f9963a70d996c19c4fc016d3ac757a0
bvcelari <bvcelari@gmail.com>
2015-08-23 19:26:18 +0200

    Pending is_active check and implementation

--- gip/utils.py
+++ gip/utils.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 
+#todo check if is_active, is not done by login_:Requiered, I guess :S
 def is_cliente(user):
     return user.groups.filter(name='cliente').exists()
 
