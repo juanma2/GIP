dc470ce911f433ed9edd519a4d6869e0be8e629d
Juan Perez <bvcelari@gmail.com>
2015-11-28 12:58:22 +0100

    updated startup for docker easy on

--- startup.sh
+++ startup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-
-if [ ! -f /var/lib/mysql/ibdata1 ]; then
-    mysql_install_db
-
-    /usr/bin/mysqld_safe &
-    sleep 10s
-
-    echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY 'changeme' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
-
-    killall mysqld
-    sleep 10s
-fi
-
-/usr/bin/mysqld_safe &
+/etc/init.d/mysql start
 #do the import here
+#!/bin/bash
+MYSQL=`which mysql`
+
+Q1="CREATE DATABASE IF NOT EXISTS gip;"
+Q2="GRANT USAGE ON *.* TO root@localhost;"
+Q3="GRANT ALL PRIVILEGES ON gip.* TO root@localhost;"
+Q4="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+
+$MYSQL -uroot  -e "$SQL"
+
+python manage.py syncdb
+#some shit breaks here, do it twice... :(
+python manage.py syncdb
+
+export DJANGO_SETTINGS_MODULE=gip.settings
+
+python carga_bd.py
