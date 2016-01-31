90e8c18bd6f1657604cae5604ee66601f41a23a8
bvcelari <bvcelari@gmail.com>
2016-01-31 11:40:34 +0000

    add some help for pedidos please

--- gip/helper_pedidos.py
+++ gip/helper_pedidos.py
@@ -1,2 +1,3 @@
 def send_order(pedidos): 
   print "implement email, or, whatever needed in gip/helper_pedidos.py"
+  print "Once the order is proccesed, should go to pending of validation or something like that"
