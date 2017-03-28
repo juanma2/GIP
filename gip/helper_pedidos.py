029c94d9dce57fd2d3b441a3a33ca4e397130d4b
bvcelari <bvcelari@gmail.com>
2017-03-28 18:36:38 +0000

    Indentation MTF

--- gip/helper_pedidos.py
+++ gip/helper_pedidos.py
@@ -38,67 +38,68 @@ def send_order(pedido,proveedor,c_pedido):
 
 
 def generate_modales_historico(pedidos):
+  print "we are tryig to get the historical pedidos"
   modales = ''
   for pedido in pedidos:
-    if int(pedido.pedidostate) in [12100]:
-      header = """
-          <div class="modal" onclick="" id="modaldemostrarpedido_{0}">
-              <div class="modal-dialog modal-lg" id="customer-order">
-                  <div class="modal-content">
-                      <div class="modal-header modal-header-info">
-                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
-                          <h4 class="modal-title">ID.Pedido: {1}</h4>
-                      </div>
-                      <div class="modal-body">
-                        <table class="table">
-                                      <thead> 
-                                          <tr>
-                                              <th>Referencia</th>
-                                              <th colspan="2">Nombre del producto</th>
+    header = """
+        <div class="modal" onclick="" id="modaldemostrarpedido_{0}">
+            <div class="modal-dialog modal-lg" id="customer-order">
+                <div class="modal-content">
+                    <div class="modal-header modal-header-info">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
+                        <h4 class="modal-title">ID.Pedido: {1}</h4>
+                    </div>
+                    <div class="modal-body">
+                      <table class="table">
+                                    <thead> 
+                                        <tr>
+                                            <th>Referencia</th>
+                                            <th colspan="2">Nombre del producto</th>
 
 
-                                              <th>Precio/ud</th>
-                                              <th>Total</th>
+                                            <th>Precio/ud</th>
+                                            <th>Total</th>
 
-                                          </tr>
-                                      </thead>
-                                      <tbody>
-      """.format(pedido.codigo,pedido.codigo)
-      products = ast.literal_eval(pedido.producto_serializado)
-      body = ''
-      for i in products['orden']:
-        body += """
-                                          <tr>
-                                              <td>{0}</td>
-                                              <td>
-                                                  <!-- <a href="s#fichamodal" data-toggle="modal">
-                                                      <img src="img/.jpg" alt="">
-                                                  </a> -->
-                                              </td>
-                                              <td><a href="s#fichamodal" data-toggle="modal">{1}</a></td>
-                                              <td>{2}</td>
-                                              <td>{3}</td>
-                                          </tr>
-          """.format(i,products['descripcion'][i].encode("utf-8"),products['orden'][i], products['orden'][i] * products['precio'][i])
-      foot = """
-                                      <tfoot>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+    """.format(pedido.codigo,pedido.codigo)
+    products = ast.literal_eval(pedido.producto_serializado)
+    body = ''
+    for i in products['orden']:
+      body += """
+                                        <tr>
+                                            <td>{0}</td>
+                                            <td>
+                                                <!-- <a href="s#fichamodal" data-toggle="modal">
+                                                    <img src="img/.jpg" alt="">
+                                                </a> -->
+                                            </td>
+                                            <td><a href="s#fichamodal" data-toggle="modal">{1}</a></td>
+                                            <td>{2}</td>
+                                            <td>{3}</td>
+                                        </tr>
+        """.format(i,products['descripcion'][i].encode("utf-8"),products['orden'][i], products['orden'][i] * products['precio'][i])
+    foot = """
+                                    <tfoot>
 
-                                          <tr>
-                                              <th colspan="4" class="text-right">Total</th>
-                                              <th>{0}</th>
-                                              <th></th>
-                                          </tr>
-                                      </tfoot>
-                                  </table>
-                      </div>
-                      <div class="modal-footer">
-                          <button type="reset" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                      </div>
-                  </div>
-              </div>
-        <!--acaba modal muestra pedido tipo 1-->
-      </div>""".format(pedido.total)
-      modales += header+body+foot
+                                        <tr>
+                                            <th colspan="4" class="text-right">Total</th>
+                                            <th>{0}</th>
+                                            <th></th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="reset" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+      <!--acaba modal muestra pedido tipo 1-->
+    </div>""".format(pedido.total)
+    modales += header+body+foot
+  print modales
   return modales
 
     
