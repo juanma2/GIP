c6ecc6e35eae8a9d64325124a93adab988ee3034
bvcelari <bvcelari@gmail.com>
2017-04-11 06:28:55 +0000

    adding estados to what a client can see

--- gip/views_cliente.py
+++ gip/views_cliente.py
@@ -481,7 +481,7 @@ def historico(request):
     username = str(current_user.username)
     current_page = "Historico"
     #check how to improve the filter, this sounds like baaaad, is the flow definition wrong, I know.
-    mis_estados = Q(pedidostate = '100') | Q (pedidostate = '12100')
+    mis_estados = Q(pedidostate = '100') | Q (pedidostate = '12100') | Q(pedidostate = '10000')
     pedidos = Pedidos.objects.filter(cliente__user=current_user.id).exclude(mis_estados)
     pedidos_estados = {}
     #is ugly, but should make jinja easier
@@ -506,7 +506,7 @@ def pedidos_en_curso(request):
     username = str(current_user.username)
     current_page = "Curso"
     #check how to improve the filter, this sounds like baaaad, is the flow definition wrong, I know.
-    mis_estados = Q(pedidostate = '100') | Q (pedidostate = '12100')
+    mis_estados = Q(pedidostate = '100') | Q (pedidostate = '12100') | Q(pedidostate = '10000' )
     search_estados = mis_estados & Q ( cliente__user= current_user.id)
     pedidos = Pedidos.objects.filter(search_estados)
     pedidos_estados = {}
