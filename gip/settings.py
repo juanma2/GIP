eca392bbbb82d9167f12f691416c9adb59f45630
juanma2 <juanmanperez@gmail.com>
2017-04-14 13:27:16 +0200

    codeship requires

--- gip/settings.py
+++ gip/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql', 
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
         'NAME': 'gip',
-        'USER': 'root',
-        'PASSWORD': 'root',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
